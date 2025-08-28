9bc65df4a5f57f3a9801f78c476d95ea6948e5ef
Rabiul <Rabiul@DESKTOP-4IJ8KBV>
2025-08-28 09:55:35 -0500

    this is Rabuil

--- src/Rabiul_functionality/Arrays.java
+++ src/Rabiul_functionality/Arrays.java
@@ -9,8 +9,10 @@ public class Arrays {
 	
 		fruits[2]="Rabiul";
 		fruits[3]="Awal";
+		fruits[4]="Bhuiyan";
 		
-		System.out.println("Number of length "+ fruits[3]);
+		
+		System.out.println("Number of length "+ fruits[3]+fruits[4]);
 
 	}
 
