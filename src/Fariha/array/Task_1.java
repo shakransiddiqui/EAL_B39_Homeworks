a1a70414ba1716f6bbe1d22a8d49b6a922e9030b
Fariha Tabassum <122464964+farihaTabassum99@users.noreply.github.com>
2025-08-28 20:10:09 -0400

    hey,this Fariha  and this is my first commit.

---
+++ src/Fariha/array/Task_1.java
@@ -0,0 +1,24 @@
+package Fariha.array;
+
+public class Task_1 {
+
+	public static void main(String[] args) {
+		
+
+		
+		String abc [] = new String [5];
+		
+	System.out.println(abc.length);
+	
+		abc[2] = "Mounia";
+		
+	for (int shakran = 0; shakran<=abc.length; shakran++) {
+		
+		System.out.println(abc[shakran]);
+	}
+				
+
+	
+	}
+
+}
