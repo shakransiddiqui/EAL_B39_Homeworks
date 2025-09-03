5b2702218f5d76a98f909903f97d6bc3662cc470
Fariha Tabassum <122464964+farihaTabassum99@users.noreply.github.com>
2025-09-03 19:32:26 -0400

    Java_task_1 fariha

---
+++ src/fariha/task/shopping_1/Amazon.java
@@ -0,0 +1,8 @@
+package shopping_1;
+
+public class Amazon extends Shopping  {
+
+	public void primeDelivery() {
+		System.out.println("Amazon Prime Delivery Available");
+	}
+}
---
+++ src/fariha/task/shopping_1/Ebay.java
@@ -0,0 +1,8 @@
+package shopping_1;
+
+public class Ebay extends Shopping {
+ 
+	public void biddingSystem() {
+		System.out.println("Ebay Items available through bidding ");
+	}
+}
